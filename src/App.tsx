18e32ca280b2135371e2bf3a596b952fe79234eb
sadeghmasoomi <68115068+sadeghmasoomi@users.noreply.github.com>
2025-04-07 08:48:18 +0330

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Card, Select } from "antd";
+import type { SelectProps } from "antd";
 
 function App() {
+  const options: SelectProps["options"] = [
+    {
+      label: "Education🎓",
+      value: "Education",
+    },
+    {
+      label: "Yeeeeah, science!",
+      value: "Yeeeeah, science!",
+    },
+    {
+      label: "Art🎭",
+      value: "Art",
+    },
+    {
+      label: "Sport⚽️",
+      value: "Sport",
+    },
+    {
+      label: "Games🎮",
+      value: "Games",
+    },
+    {
+      label: "Health🏥",
+      value: "Health",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className="card">
+      <Select
+        className="select-box"
+        mode="tags"
+        style={{ width: "100%" }}
+        placeholder="Science"
+        options={options}
+      />
+    </Card>
   );
 }
 
